77b95ce2c8f99b9cba14e9380939082b6b6dfbe6
Marian Dziubiak <marian.dziubiak@gmail.com>
2021-06-12 16:41:20 +0100

    Input customization (editor)

--- ProjectStepUp/Environment/TriggerSwitch.cs
+++ ProjectStepUp/Environment/TriggerSwitch.cs
@@ -14,9 +14,12 @@ namespace ProjectStepUp
     public class TriggerSwitch : SyncScript
     {
         public static readonly EventKey<(string, SwitchState)> SwitchStateChange = new("General");
+        public static readonly EventKey SwitchToggle = new("General", "SwitchToggle");
+
         private StaticColliderComponent sc;
         private Entity lever;
         private SwitchState state = SwitchState.OFF;
+        private EventReceiver toggleEvent = new EventReceiver(SwitchToggle);
         private bool StateBool {get {return state == SwitchState.ON;}}
 
         public override void Start()
@@ -27,9 +30,9 @@ namespace ProjectStepUp
 
         public override void Update()
         {
-            if(sc.Collisions.Any(x => x.ColliderA is CharacterComponent || x.ColliderB is CharacterComponent))
+            if (sc.Collisions.Any(x => x.ColliderA is CharacterComponent || x.ColliderB is CharacterComponent))
             {
-                if(Input.IsKeyPressed(Keys.U))
+                if (toggleEvent.TryReceive())
                 {
                     state = state == SwitchState.ON ? SwitchState.OFF : SwitchState.OFF;
                     SwitchStateChange.Broadcast((Entity.GetParent()?.Name,state));
