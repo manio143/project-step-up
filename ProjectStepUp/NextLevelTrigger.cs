326539fdf0a2f18bcad10000cc7980ee79f57757
Marian Dziubiak <marian.dziubiak@gmail.com>
2021-06-13 17:35:52 +0100

    Add visual effect to the next level trigger and require both characters to contact it

--- ProjectStepUp/NextLevelTrigger.cs
+++ ProjectStepUp/NextLevelTrigger.cs
@@ -1,19 +1,36 @@
 ﻿using System.Threading.Tasks;
+using ProjectStepUp.Character;
 using Stride.Engine;
+using Stride.Physics;
 
 namespace ProjectStepUp
 {
     public class NextLevelTrigger : AsyncScript
     {
+        private bool lightPassed;
+        private bool heavyPassed;
         public int NextLevelIndex { get; set; }
 
         public override async Task Execute()
         {
             var physics = Entity.Get<PhysicsComponent>();
 
-            _ = await physics.NewCollision();
+            do
+            {
+                var collision = await physics.NewCollision();
+                var character = collision.ColliderA is CharacterComponent ? collision.ColliderA.Entity : collision.ColliderB.Entity;
+                var energyType = character.Get<CharacterController>().Energy.Type;
 
-            LevelSceneManager.LoadLevelRequest.Broadcast(NextLevelIndex);
+                if (energyType == EnergyType.Light) lightPassed = true;
+                if (energyType == EnergyType.Heavy) heavyPassed = true;
+
+                if (Check())
+                {
+                    LevelSceneManager.LoadLevelRequest.Broadcast(NextLevelIndex);
+                }
+            } while (!Check());
         }
+
+        private bool Check() => lightPassed && heavyPassed;
     }
 }
